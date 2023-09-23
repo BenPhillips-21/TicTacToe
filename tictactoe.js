251ea8e7d7edfd37c3a950e7116b34b2e96741e3
HolidayGuy21 <14449@coderacademy.edu.au>
2023-09-23 16:08:48 +1000

    finished initial styling

---
+++ tictactoe.js
@@ -0,0 +1,16 @@
+const gameBoard = (() => {
+    let board = ["X", "X", "X", "O", "O", "X", "X", "O", "O"]
+    return {board}
+})()
+
+const displayBoard = ((board) => {
+
+})()
+
+function Player(name) {
+    
+}
+
+const playerOne = Player()
+const playerTwo = Player()
+

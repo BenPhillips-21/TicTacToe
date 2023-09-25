b5e60cf4ca0b1efa2d91b5f7d0d9a9369c0e536a
HolidayGuy21 <14449@coderacademy.edu.au>
2023-09-25 14:53:46 +1000

    created checkScore function, created player and playerRole

--- tictactoe.js
+++ tictactoe.js
@@ -22,29 +22,69 @@ const displayBoard = (() => {
     spaceNine.innerHTML = gameBoard[8]
 })()
 
+const checkScore = (gameBoard) => {
+    console.log("checkScore invoked");
+    console.log(gameBoard);
+    const areAllFull = gameBoard.every(item => item !== "");
+    for (let i = 0; i < gameBoard.length; i++) {
+        if (areAllFull) {
+            console.log("It's a draw!")
+            // DRAWS += 1
+        }
+        else if (gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X" || gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X" || gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X" || gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X" || gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X" || gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X" || gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X" || gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X") {
+            console.log("X wins!");
+            // PLAYER X WINS += 1
+        }
+        else if (gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard[2] === "O" || gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard[8] === "O" || gameBoard[2] === "O" && gameBoard[4] === "O" && gameBoard[6] === "O" || gameBoard[1] === "O" && gameBoard[4] === "O" && gameBoard[7] === "O" || gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O" || gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O" || gameBoard[6] === "O" && gameBoard[7] === "O" && gameBoard[8] === "O" || gameBoard[0] === "O" && gameBoard[4] === "O" && gameBoard[8] === "O") {
+            console.log("O wins!");
+            // PLAYER O WINS += 1
+        }
+        else {
+            console.log("breaking!");
+            break
+        }
+    }
+};
+
+
 function addMark(gameBoard, space) {
     console.log("addMark invoked")
-    console.log(space)
-    space.children[0].innerHTML = "X"
-    let spaceIndex = space.firstChild.attributes[1].value
-    gameBoard.splice(spaceIndex, 1, "X")
-    console.log(gameBoard)
+    const areAllStringsEmpty = gameBoard.every(str => str.trim() === "")
+        if (areAllStringsEmpty) {
+            space.children[0].innerHTML = "X"
+            let spaceIndex = space.firstChild.attributes[1].value
+            gameBoard.splice(spaceIndex, 1, "X")
+            console.log(gameBoard)
+        } else if (gameBoard) {
+            space.children[0].innerHTML = "X"
+            let spaceIndex = space.firstChild.attributes[1].value
+            gameBoard.splice(spaceIndex, 1, "X")
+            console.log(gameBoard)
+        }
 }
 
-function Player(name) {
+// function reset() {
+//     reset game board
+// }
+
+spaceOne.onclick = () => { addMark(gameBoard, spaceOne), checkScore(gameBoard) };
+spaceTwo.onclick = () => { addMark(gameBoard, spaceTwo), checkScore(gameBoard) };
+spaceThree.onclick = () => { addMark(gameBoard, spaceThree), checkScore(gameBoard) };
+spaceFour.onclick = () => { addMark(gameBoard, spaceFour), checkScore(gameBoard) };
+spaceFive.onclick = () => { addMark(gameBoard, spaceFive), checkScore(gameBoard) };
+spaceSix.onclick = () => { addMark(gameBoard, spaceSix), checkScore(gameBoard) };
+spaceSeven.onclick = () => { addMark(gameBoard, spaceSeven), checkScore(gameBoard) };
+spaceEight.onclick = () => { addMark(gameBoard, spaceEight), checkScore(gameBoard) };
+spaceNine.onclick = () => { addMark(gameBoard, spaceNine), checkScore(gameBoard) };
+
+const Player = role => {
+    let playerRole = role
+    return {playerRole}
 }
 
-spaceOne.onclick = () => { addMark(gameBoard, spaceOne) };
-spaceTwo.onclick = () => { addMark(gameBoard, spaceTwo) };
-spaceThree.onclick = () => { addMark(gameBoard, spaceThree) };
-spaceFour.onclick = () => { addMark(gameBoard, spaceFour) };
-spaceFive.onclick = () => { addMark(gameBoard, spaceFive) };
-spaceSix.onclick = () => { addMark(gameBoard, spaceSix) };
-spaceSeven.onclick = () => { addMark(gameBoard, spaceSeven) };
-spaceEight.onclick = () => { addMark(gameBoard, spaceEight) };
-spaceNine.onclick = () => { addMark(gameBoard, spaceNine) };
+const playerX = Player("X")
+const playerO = Player("O")
 
-
-const playerOne = Player()
-const playerTwo = Player()
+console.log(playerX.playerRole)
+console.log(playerO.playerRole)
 

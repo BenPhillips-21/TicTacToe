e07c9bbae686d9f47cd8397b8288c7f496f4ed1e
HolidayGuy21 <14449@coderacademy.edu.au>
2023-09-25 16:26:42 +1000

    made displayBoard into a function, finished addMark, added reset function

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,6 @@
 const gameBoard = ["", "", "", "", "", "", "", "", ""]
 
-const displayBoard = (() => {
-    console.log(gameBoard)
+function displayBoard() {
     const spaceOne = document.querySelector('.content[data-index="0"]');
     const spaceTwo = document.querySelector('.content[data-index="1"]');
     const spaceThree = document.querySelector('.content[data-index="2"]');
@@ -11,16 +10,16 @@ const displayBoard = (() => {
     const spaceSeven = document.querySelector('.content[data-index="6"]');
     const spaceEight = document.querySelector('.content[data-index="7"]');
     const spaceNine = document.querySelector('.content[data-index="8"]');
-    spaceOne.innerHTML = gameBoard[0]
-    spaceTwo.innerHTML = gameBoard[1]
-    spaceThree.innerHTML = gameBoard[2]
-    spaceFour.innerHTML = gameBoard[3]
-    spaceFive.innerHTML = gameBoard[4]
-    spaceSix.innerHTML = gameBoard[5]
-    spaceSeven.innerHTML = gameBoard[6]
-    spaceEight.innerHTML = gameBoard[7]
-    spaceNine.innerHTML = gameBoard[8]
-})()
+    spaceOne.innerHTML = gameBoard[0];
+    spaceTwo.innerHTML = gameBoard[1];
+    spaceThree.innerHTML = gameBoard[2];
+    spaceFour.innerHTML = gameBoard[3];
+    spaceFive.innerHTML = gameBoard[4];
+    spaceSix.innerHTML = gameBoard[5];
+    spaceSeven.innerHTML = gameBoard[6];
+    spaceEight.innerHTML = gameBoard[7];
+    spaceNine.innerHTML = gameBoard[8];
+}
 
 const checkScore = (gameBoard) => {
     console.log("checkScore invoked");
@@ -30,14 +29,17 @@ const checkScore = (gameBoard) => {
         if (areAllFull) {
             console.log("It's a draw!")
             // DRAWS += 1
+            reset(gameBoard)
         }
         else if (gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X" || gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X" || gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X" || gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X" || gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X" || gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X" || gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X" || gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X") {
             console.log("X wins!");
             // PLAYER X WINS += 1
+            reset(gameBoard)
         }
         else if (gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard[2] === "O" || gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard[8] === "O" || gameBoard[2] === "O" && gameBoard[4] === "O" && gameBoard[6] === "O" || gameBoard[1] === "O" && gameBoard[4] === "O" && gameBoard[7] === "O" || gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O" || gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O" || gameBoard[6] === "O" && gameBoard[7] === "O" && gameBoard[8] === "O" || gameBoard[0] === "O" && gameBoard[4] === "O" && gameBoard[8] === "O") {
             console.log("O wins!");
             // PLAYER O WINS += 1
+            reset(gameBoard)
         }
         else {
             console.log("breaking!");
@@ -49,23 +51,42 @@ const checkScore = (gameBoard) => {
 
 function addMark(gameBoard, space) {
     console.log("addMark invoked")
+    console.log(gameBoard)
     const areAllStringsEmpty = gameBoard.every(str => str.trim() === "")
+    const X = "X"
+    const O = "O"
+    const Xs = gameBoard.filter(str => str === X).length
+    const Os = gameBoard.filter(str => str === O).length
+    console.log(Xs)
+    console.log(Os)
         if (areAllStringsEmpty) {
             space.children[0].innerHTML = "X"
             let spaceIndex = space.firstChild.attributes[1].value
             gameBoard.splice(spaceIndex, 1, "X")
             console.log(gameBoard)
-        } else if (gameBoard) {
+        } else if (Xs === Os) {
             space.children[0].innerHTML = "X"
             let spaceIndex = space.firstChild.attributes[1].value
             gameBoard.splice(spaceIndex, 1, "X")
             console.log(gameBoard)
+        } else if (Os < Xs) {
+            space.children[0].innerHTML = "O"
+            let spaceIndex = space.firstChild.attributes[1].value
+            gameBoard.splice(spaceIndex, 1, "O")
+            console.log(gameBoard)
         }
 }
 
-// function reset() {
-//     reset game board
-// }
+function reset(gameBoard) {
+    for (let i = 0; i < gameBoard.length; i++) {
+        if (typeof gameBoard[i] === "string") {
+            gameBoard[i] = "";
+        }
+    }
+    console.log(gameBoard)
+    console.log("RESET")
+    displayBoard()
+}
 
 spaceOne.onclick = () => { addMark(gameBoard, spaceOne), checkScore(gameBoard) };
 spaceTwo.onclick = () => { addMark(gameBoard, spaceTwo), checkScore(gameBoard) };
@@ -87,4 +108,6 @@ const playerO = Player("O")
 
 console.log(playerX.playerRole)
 console.log(playerO.playerRole)
+
+displayBoard()
 

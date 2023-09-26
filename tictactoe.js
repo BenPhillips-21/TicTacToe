29f472e7e79fdc3c343e355b2112ffe309936dd6
HolidayGuy21 <14449@coderacademy.edu.au>
2023-09-26 18:07:29 +1000

    added favicon, fixed scoreboard, added winning messages, added turn tracker, added overlay that pauses screen for 2 seconds after game, cleaned up container event listener, cleaned up display board function

--- tictactoe.js
+++ tictactoe.js
@@ -1,115 +1,108 @@
 const gameBoard = ["", "", "", "", "", "", "", "", ""]
 
+let isXTurn = true 
+
 function updateScoresX() {
     let playerXScore = document.getElementById("playerXWins")
-    playerXScore.firstChild.lastChild.innerHTML = playerX.playerWins
+    playerXScore.firstChild.lastChild.innerHTML = Math.round(playerX.playerWins)
 }
 
 function updateScoresO() {
     let playerOScore = document.getElementById("playerOWins")
-    playerOScore.firstChild.lastChild.innerHTML = playerO.playerWins
+    playerOScore.firstChild.lastChild.innerHTML = Math.round(playerO.playerWins)
 }
 
 function displayBoard() {
-    const spaceOne = document.querySelector('.content[data-index="0"]');
-    const spaceTwo = document.querySelector('.content[data-index="1"]');
-    const spaceThree = document.querySelector('.content[data-index="2"]');
-    const spaceFour = document.querySelector('.content[data-index="3"]');
-    const spaceFive = document.querySelector('.content[data-index="4"]');
-    const spaceSix = document.querySelector('.content[data-index="5"]');
-    const spaceSeven = document.querySelector('.content[data-index="6"]');
-    const spaceEight = document.querySelector('.content[data-index="7"]');
-    const spaceNine = document.querySelector('.content[data-index="8"]');
-    spaceOne.innerHTML = gameBoard[0];
-    spaceTwo.innerHTML = gameBoard[1];
-    spaceThree.innerHTML = gameBoard[2];
-    spaceFour.innerHTML = gameBoard[3];
-    spaceFive.innerHTML = gameBoard[4];
-    spaceSix.innerHTML = gameBoard[5];
-    spaceSeven.innerHTML = gameBoard[6];
-    spaceEight.innerHTML = gameBoard[7];
-    spaceNine.innerHTML = gameBoard[8];
+    for (let i = 0; i < gameBoard.length; i++) {
+        const space = document.querySelector(`.content[data-index="${i}"]`)
+        space.innerHTML = gameBoard[i]
+    }
 }
 
 const checkScore = (gameBoard) => {
-    console.log("checkScore invoked");
-    console.log(gameBoard);
-    const areAllFull = gameBoard.every(item => item !== "");
+    const areAllFull = gameBoard.every(item => item !== "")
     for (let i = 0; i < gameBoard.length; i++) {
         if (gameBoard[0] === "X" && gameBoard[1] === "X" && gameBoard[2] === "X" || gameBoard[2] === "X" && gameBoard[5] === "X" && gameBoard[8] === "X" || gameBoard[2] === "X" && gameBoard[4] === "X" && gameBoard[6] === "X" || gameBoard[1] === "X" && gameBoard[4] === "X" && gameBoard[7] === "X" || gameBoard[0] === "X" && gameBoard[3] === "X" && gameBoard[6] === "X" || gameBoard[3] === "X" && gameBoard[4] === "X" && gameBoard[5] === "X" || gameBoard[6] === "X" && gameBoard[7] === "X" && gameBoard[8] === "X" || gameBoard[0] === "X" && gameBoard[4] === "X" && gameBoard[8] === "X") {
-            console.log("X wins!");
-            playerX.playerWins += 1
-            console.log(playerX)
-            reset(gameBoard)
-            updateScoresX()
-        }
-        else if (gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard[2] === "O" || gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard[8] === "O" || gameBoard[2] === "O" && gameBoard[4] === "O" && gameBoard[6] === "O" || gameBoard[1] === "O" && gameBoard[4] === "O" && gameBoard[7] === "O" || gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O" || gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O" || gameBoard[6] === "O" && gameBoard[7] === "O" && gameBoard[8] === "O" || gameBoard[0] === "O" && gameBoard[4] === "O" && gameBoard[8] === "O") {
-            console.log("O wins!");
-            playerO.playerWins += 1
-            console.log(playerO)
-            reset(gameBoard)
-            updateScoresO()
+            playerX.playerWins += 1/9
+            var heading = document.getElementById("hiddenHeading")
+            heading.style.display = "block"
+            var overlay = document.getElementById("overlay")
+            overlay.style.display = "block"
+            setTimeout(function() {
+                heading.style.display = "none"
+                overlay.style.display = "none"
+                toggleTurnHeading()
+                updateScoresX() 
+                reset(gameBoard)
+            }, 2000)
+            toggleTurnHeadingOff()           
+        } else if (gameBoard[0] === "O" && gameBoard[1] === "O" && gameBoard[2] === "O" || gameBoard[2] === "O" && gameBoard[5] === "O" && gameBoard[8] === "O" || gameBoard[2] === "O" && gameBoard[4] === "O" && gameBoard[6] === "O" || gameBoard[1] === "O" && gameBoard[4] === "O" && gameBoard[7] === "O" || gameBoard[0] === "O" && gameBoard[3] === "O" && gameBoard[6] === "O" || gameBoard[3] === "O" && gameBoard[4] === "O" && gameBoard[5] === "O" || gameBoard[6] === "O" && gameBoard[7] === "O" && gameBoard[8] === "O" || gameBoard[0] === "O" && gameBoard[4] === "O" && gameBoard[8] === "O") {
+            playerO.playerWins += 1/9
+            var heading = document.getElementById("hiddenHeadingTwo")
+            heading.style.display = "block"
+            var overlay = document.getElementById("overlay")
+            overlay.style.display = "block"
+            setTimeout(function() {
+                heading.style.display = "none"
+                overlay.style.display = "none"
+                toggleTurnHeading()
+                updateScoresO()
+                reset(gameBoard)
+            }, 2000)
+            toggleTurnHeadingOff()
         } else if (areAllFull) {
-            console.log("It's a draw!");
-            reset(gameBoard);
+            var heading = document.getElementById("hiddenHeadingThree")
+            heading.style.display = "block"
+            var overlay = document.getElementById("overlay")
+            overlay.style.display = "block"
+            setTimeout(function() {
+                heading.style.display = "none"
+                overlay.style.display = "none"
+                toggleTurnHeading()
+                reset(gameBoard)
+            }, 2000)
+            toggleTurnHeadingOff()
         }
         else {
-            console.log("breaking!");
             break
         }
     }
-};
+}
 
 function addMark(gameBoard, space) {
-    console.log("addMark invoked")
-    console.log(gameBoard)
-    const areAllStringsEmpty = gameBoard.every(str => str.trim() === "")
-    const X = "X"
-    const O = "O"
-    const Xs = gameBoard.filter(str => str === X).length
-    const Os = gameBoard.filter(str => str === O).length
-    console.log(Xs)
-    console.log(Os)
-        if (areAllStringsEmpty) {
+        if (isXTurn) {
             space.children[0].innerHTML = "X"
             let spaceIndex = space.firstChild.attributes[1].value
             gameBoard.splice(spaceIndex, 1, "X")
-            console.log(gameBoard)
-        } else if (space.children[0].innerHTML === "X" || space.children[0].innerHTML === "O") {       
-            console.log("Next")
-        } else if (Xs === Os) {
-            space.children[0].innerHTML = "X"
-            let spaceIndex = space.firstChild.attributes[1].value
-            gameBoard.splice(spaceIndex, 1, "X")
-            console.log(gameBoard)
-        } else if (Os < Xs) {
+            isXTurn = !isXTurn
+            toggleTurnHeading()
+        } else {
             space.children[0].innerHTML = "O"
             let spaceIndex = space.firstChild.attributes[1].value
             gameBoard.splice(spaceIndex, 1, "O")
-            console.log(gameBoard)
+            isXTurn = !isXTurn
+            toggleTurnHeading()
         }
 }
 
 function reset(gameBoard) { 
     for (let i = 0; i < gameBoard.length; i++) {
         if (typeof gameBoard[i] === "string") {
-            gameBoard[i] = "";
+            gameBoard[i] = ""
         }
     }
-    console.log(gameBoard)
-    console.log("RESET")
     displayBoard()
 }
 
-spaceOne.onclick = () => { addMark(gameBoard, spaceOne), checkScore(gameBoard) };
-spaceTwo.onclick = () => { addMark(gameBoard, spaceTwo), checkScore(gameBoard) };
-spaceThree.onclick = () => { addMark(gameBoard, spaceThree), checkScore(gameBoard) };
-spaceFour.onclick = () => { addMark(gameBoard, spaceFour), checkScore(gameBoard) };
-spaceFive.onclick = () => { addMark(gameBoard, spaceFive), checkScore(gameBoard) };
-spaceSix.onclick = () => { addMark(gameBoard, spaceSix), checkScore(gameBoard) };
-spaceSeven.onclick = () => { addMark(gameBoard, spaceSeven), checkScore(gameBoard) };
-spaceEight.onclick = () => { addMark(gameBoard, spaceEight), checkScore(gameBoard) };
-spaceNine.onclick = () => { addMark(gameBoard, spaceNine), checkScore(gameBoard) };
+const container = document.getElementById("container")
+container.addEventListener("click", function(event) {
+    const target = event.target
+    
+    if (target.classList.contains("clickable-div")) {
+        addMark(gameBoard, target)
+        checkScore(gameBoard)
+    }
+})
 
 resetButton.onclick = () => { reset(gameBoard) }
 
@@ -121,10 +114,28 @@ const Player = (role, wins) => {
 
 const playerX = Player("X", 0)
 const playerO = Player("O", 0)
-const drawCount = Draw(0)
-
-console.log(playerX.playerRole)
-console.log(playerO.playerRole)
 
 displayBoard()
+
+function toggleTurnHeading() {
+    const headingX = document.getElementById("hiddenHeadingFour")
+    const headingO = document.getElementById("hiddenHeadingFive")
+
+    if (isXTurn) {
+        headingX.style.display = "block"
+        headingO.style.display = "none"
+    } else {
+        headingX.style.display = "none"
+        headingO.style.display = "block"
+    }
+}
+
+function toggleTurnHeadingOff() { 
+    const headingX = document.getElementById("hiddenHeadingFour")
+    const headingO = document.getElementById("hiddenHeadingFive")
+    headingO.style.display = "none"
+    headingX.style.display = "none"
+}
+
+toggleTurnHeading()
 
